f530bec5115a2c1375face1201081b60f91a0b93
rasya-vue <rasya.vue@outlook.com>
2023-05-08 01:15:37 +0700

    update

--- cstore.c
+++ cstore.c
@@ -5,13 +5,15 @@ typedef struct
 {
     char nama_barang;
     char harga, stock;
-} Smartphone;
+} Aksesoris;
 
 typedef struct
 {
     char nama_barang;
     char harga, stock;
-} Aksesoris;
+} Smartphone;
 
-
-
+int main {
+    printf ("Selamat datang di Store Kami");
+    printf ("Pilih 'A' untuk Aksesoris\n atau 'S' untuk Smartphone");
+}
