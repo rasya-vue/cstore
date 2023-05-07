f5f216205dbee04d9f92f224c714fd0850459ad5
rasya-vue <rasya.vue@outlook.com>
2023-05-08 01:11:02 +0700

    update

--- cstore.c
+++ cstore.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct
+{
+    char nama_barang;
+    char harga, stock;
+} Smartphone;
+
+typedef struct
+{
+    char nama_barang;
+    char harga, stock;
+} Aksesoris;
+
+
+
